4a4f466772964787f779581cdd3e757efad8b091
alban <albanosdes@gmail.com>
2020-11-18 21:52:08 +0000

    Curl

--- github-release.sh
+++ github-release.sh
@@ -1,36 +1,15 @@
 #!/usr/bin/env bash
 set -euo pipefail
-
-PATH=${PATH}:/usr/local/go/bin
 GENERATED_FILE=pqchecker_${PQCHECKER_VERSION}-1_${ARCH}
-github-release --version
 
-if [ $(github-release info --user ${GITHUB_USER} --repo ${GITHUB_REPOSITORY} --tag v${PQCHECKER_VERSION}|grep v${PQCHECKER_VERSION}|grep -v commit|wc -l) -eq 0 ];
-then
-  github-release -v release \
-      --user ${GITHUB_USER} \
-      --repo ${GITHUB_REPOSITORY} \
-      --tag v${PQCHECKER_VERSION} \
-      --name "pqChecker-${PQCHECKER_VERSION}" \
-      --description "Multiarch pqChecker debian package."
+RELEASE=$(curl -XPOST -H "Authorization:token ${GITHUB_TOKEN}" --data "{\"tag_name\": \"v${PQCHECKER_VERSION}\", \"target_commitish\": \"master\", \"name\": \"pqChecker v${PQCHECKER_VERSION}\", \"body\": \"pqChecker Debian package\", \"draft\": false, \"prerelease\": false}" https://api.github.com/repos/${GITHUB_USER}/${GITHUB_REPOSITORY}/releases)
 
-      # wait for the release to be created
-      # otherwise the upload will fail
-      sleep 5
-fi
+RELEASE_ID=$(echo "${RELEASE}" | sed -n -e 's/"id":\ \([0-9]\+\),/\1/p' | head -n 1 | sed 's/[[:blank:]]//g')
+
+echo "Release id : ${RELEASE_ID}"
 
 sha1sum /tmp/${GENERATED_FILE}.deb > /tmp/${GENERATED_FILE}.sha1sum
 
-github-release -v upload \
-    --user ${GITHUB_USER} \
-    --repo ${GITHUB_REPOSITORY} \
-    --tag v${PQCHECKER_VERSION} \
-    --name "${GENERATED_FILE}.deb" \
-    --file /tmp/${GENERATED_FILE}.deb
+curl -XPOST -H "Authorization:token ${GITHUB_TOKEN}" -H "Content-Type:application/octet-stream" --data-binary /tmp/${GENERATED_FILE}.deb https://uploads.github.com/repos/${GITHUB_USER}/${GITHUB_REPOSITORY}/releases/${RELEASE_ID}/assets?name=${GENERATED_FILE}.deb
 
-github-release -v upload \
-    --user ${GITHUB_USER} \
-    --repo ${GITHUB_REPOSITORY} \
-    --tag v${PQCHECKER_VERSION} \
-    --name "${GENERATED_FILE}.sha1sum" \
-    --file /tmp/${GENERATED_FILE}.sha1sum
+curl -XPOST -H "Authorization:token ${GITHUB_TOKEN}" -H "Content-Type:application/octet-stream" --data-binary /tmp/${GENERATED_FILE}.sha1sum https://uploads.github.com/repos/${GITHUB_USER}/${GITHUB_REPOSITORY}/releases/${RELEASE_ID}/assets?name=${GENERATED_FILE}.sha1sum
