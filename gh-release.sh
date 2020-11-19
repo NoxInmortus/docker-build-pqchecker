c21fe1d32cbe02e0e8bf70437b1dcf1f72e301a2
NoxInmortus <noxinmortus@protonmail.com>
2020-11-19 15:00:14 +0100

    fix

--- gh-release.sh
+++ gh-release.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 set -euo pipefail
-set -x
 PQCHECKER_VERSION=$(grep 'ENV PQCHECKER_VERSION' Dockerfile |awk -F\' '{ print $2 }')
 GITHUB_REPOSITORY=${CI_PROJECT_NAME}
 GITHUB_RELEASE_NAME="pqChecker v${PQCHECKER_VERSION}"
@@ -8,6 +7,7 @@ GITHUB_RELEASE_NAME="pqChecker v${PQCHECKER_VERSION}"
 GITHUB_RELEASES_CHECK=$(curl -s https://api.github.com/repos/${GITHUB_USER}/${GITHUB_REPOSITORY}/releases|grep 'tag_name'|grep ${PQCHECKER_VERSION}|wc -l)
 
 if [ ${GITHUB_RELEASES_CHECK} -eq 0 ];
+then
   echo 'Creating GITHUB release...'
   GITHUB_RELEASE=$(curl -s -XPOST -H "Authorization:token ${GITHUB_TOKEN}" --data "{\"tag_name\": \"v${PQCHECKER_VERSION}\", \"target_commitish\": \"master\", \"name\": \"${GITHUB_RELEASE_NAME}\", \"body\": \"pqChecker Debian package\", \"draft\": false, \"prerelease\": false}" https://api.github.com/repos/${GITHUB_USER}/${GITHUB_REPOSITORY}/releases)
   GITHUB_RELEASE_ID=$(echo "${GITHUB_RELEASE}" | sed -n -e 's/"id":\ \([0-9]\+\),/\1/p' | head -n 1 | sed 's/[[:blank:]]//g')
