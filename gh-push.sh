434217cb5138c8463bfbd7332ae0f9b6e30198d5
NoxInmortus <noxinmortus@protonmail.com>
2020-11-19 15:35:02 +0100

    wip

--- gh-push.sh
+++ gh-push.sh
@@ -11,5 +11,6 @@ echo "Release id : ${GITHUB_RELEASE_ID}"
 sha1sum /tmp/${GENERATED_FILE}.deb > /tmp/${GENERATED_FILE}.sha1sum
 
 curl -s -XPOST -H "Authorization:token ${GITHUB_TOKEN}" -H "Content-Type:application/octet-stream" --data-binary /tmp/${GENERATED_FILE}.deb https://uploads.github.com/repos/${GITHUB_USER}/${GITHUB_REPOSITORY}/releases/${GITHUB_RELEASE_ID}/assets?name=${GENERATED_FILE}.deb
-
 curl -s -XPOST -H "Authorization:token ${GITHUB_TOKEN}" -H "Content-Type:application/octet-stream" --data-binary /tmp/${GENERATED_FILE}.sha1sum https://uploads.github.com/repos/${GITHUB_USER}/${GITHUB_REPOSITORY}/releases/${GITHUB_RELEASE_ID}/assets?name=${GENERATED_FILE}.sha1sum
+
+exit 0
--- gh-release.sh
+++ gh-release.sh
@@ -7,7 +7,7 @@ GITHUB_RELEASE_NAME="pqChecker v${PQCHECKER_VERSION}-$(date '+%d%m%Y-%Hh%Mm')"
 
 GITHUB_RELEASES_CHECK=$(curl -s https://api.github.com/repos/${GITHUB_USER}/${GITHUB_REPOSITORY}/releases|grep 'tag_name'|grep ${PQCHECKER_VERSION}|wc -l)
 
-if [ ${GITHUB_RELEASES_CHECK} -eq 0 ];
+if [[ ${GITHUB_RELEASES_CHECK} -eq 0 ]];
 then
   echo 'Creating GITHUB release...'
   GITHUB_RELEASE=$(curl -sS -XPOST -H "Authorization:token ${GITHUB_TOKEN}" --data "{\"tag_name\": \"v${PQCHECKER_VERSION}\", \"target_commitish\": \"master\", \"name\": \"${GITHUB_RELEASE_NAME}\", \"body\": \"pqChecker Debian package\", \"draft\": false, \"prerelease\": false}" https://api.github.com/repos/${GITHUB_USER}/${GITHUB_REPOSITORY}/releases)
