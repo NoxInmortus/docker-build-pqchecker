24bb60cbc93e0676b382a46d0ff65e5a20412f18
NoxInmortus <noxinmortus@protonmail.com>
2020-11-19 15:38:25 +0100

    test

--- gh-release.sh
+++ gh-release.sh
@@ -10,7 +10,7 @@ GITHUB_RELEASES_CHECK=$(curl -s https://api.github.com/repos/${GITHUB_USER}/${GI
 if [[ ${GITHUB_RELEASES_CHECK} -eq 0 ]];
 then
   echo 'Creating GITHUB release...'
-  GITHUB_RELEASE=$(curl -sS -XPOST -H "Authorization:token ${GITHUB_TOKEN}" --data "{\"tag_name\": \"v${PQCHECKER_VERSION}\", \"target_commitish\": \"master\", \"name\": \"${GITHUB_RELEASE_NAME}\", \"body\": \"pqChecker Debian package\", \"draft\": false, \"prerelease\": false}" https://api.github.com/repos/${GITHUB_USER}/${GITHUB_REPOSITORY}/releases)
+  #GITHUB_RELEASE=$(curl -sS -XPOST -H "Authorization:token ${GITHUB_TOKEN}" --data "{\"tag_name\": \"v${PQCHECKER_VERSION}\", \"target_commitish\": \"master\", \"name\": \"${GITHUB_RELEASE_NAME}\", \"body\": \"pqChecker Debian package\", \"draft\": false, \"prerelease\": false}" https://api.github.com/repos/${GITHUB_USER}/${GITHUB_REPOSITORY}/releases)
   echo "GITHUB release ${GITHUB_RELEASE_NAME} created."
 else
   echo 'GITHUB release not created. A GITHUB release named ${GITHUB_RELEASE_NAME} already exist.'
