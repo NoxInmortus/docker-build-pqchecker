34f347b2b0d6fcd6ccc25d687c65f2437ae700ee
NoxInmortus <noxinmortus@protonmail.com>
2020-11-19 12:08:11 +0100

    wip

--- gh-push.sh
+++ gh-push.sh
@@ -3,6 +3,8 @@ set -euo pipefail
 
 GENERATED_FILE=pqchecker_${PQCHECKER_VERSION}-1_${ARCH}
 
+GITHUB_RELEASE_ID=$(curl -s https://api.github.com/repos/${GITHUB_USER}/${GITHUB_REPOSITORY}/releases/latest|grep '\"url\"'|head -n 1|awk -F\" '{ print $(NF-1) }'|awk -F/ '{ print $NF }')
+
 echo "Release id : ${GITHUB_RELEASE_ID}"
 
 sha1sum /tmp/${GENERATED_FILE}.deb > /tmp/${GENERATED_FILE}.sha1sum
--- gh-release.sh
+++ gh-release.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -euo pipefail
-PQCHECKER_VERSION=${1}
+PQCHECKER_VERSION=$(grep 'ENV PQCHECKER_VERSION' Dockerfile |awk -F\' '{ print $2 }')
 GITHUB_REPOSITORY=${CI_PROJECT_NAME}
 
 GITHUB_RELEASE=$(curl -s -XPOST -H "Authorization:token ${GITHUB_TOKEN}" --data "{\"tag_name\": \"v${PQCHECKER_VERSION}\", \"target_commitish\": \"master\", \"name\": \"pqChecker v${PQCHECKER_VERSION}\", \"body\": \"pqChecker Debian package\", \"draft\": false, \"prerelease\": false}" https://api.github.com/repos/${GITHUB_USER}/${GITHUB_REPOSITORY}/releases)
