23ea1a84aa2352153e7814e6e2f77247ce0887fb
NoxInmortus <noxinmortus@protonmail.com>
2020-11-19 10:50:17 +0100

    wip

--- gh-release.sh
+++ gh-release.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -euo pipefail
+GITHUB_TOKEN
 PQCHECKER_VERSION=${1}
+GITHUB_REPOSITORY=${CI_PROJECT_NAME}
 
 GITHUB_RELEASE=$(curl -s --no-progress-meter -XPOST -H "Authorization:token ${GITHUB_TOKEN}" --data "{\"tag_name\": \"v${PQCHECKER_VERSION}\", \"target_commitish\": \"master\", \"name\": \"pqChecker v${PQCHECKER_VERSION}\", \"body\": \"pqChecker Debian package\", \"draft\": false, \"prerelease\": false}" https://api.github.com/repos/${GITHUB_USER}/${GITHUB_REPOSITORY}/releases)
 
