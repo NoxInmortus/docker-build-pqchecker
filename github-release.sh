8157c4d3148ae56bc8cba0363fdf7a69cb1b428e
alban <albanosdes@gmail.com>
2020-11-19 08:56:30 +0000

    Curl

--- github-release.sh
+++ github-release.sh
@@ -2,10 +2,6 @@
 set -euo pipefail
 GENERATED_FILE=pqchecker_${PQCHECKER_VERSION}-1_${ARCH}
 
-RELEASE=$(curl -XPOST -H "Authorization:token ${GITHUB_TOKEN}" --data "{\"tag_name\": \"v${PQCHECKER_VERSION}\", \"target_commitish\": \"master\", \"name\": \"pqChecker v${PQCHECKER_VERSION}\", \"body\": \"pqChecker Debian package\", \"draft\": false, \"prerelease\": false}" https://api.github.com/repos/${GITHUB_USER}/${GITHUB_REPOSITORY}/releases)
-
-RELEASE_ID=$(echo "${RELEASE}" | sed -n -e 's/"id":\ \([0-9]\+\),/\1/p' | head -n 1 | sed 's/[[:blank:]]//g')
-
 echo "Release id : ${RELEASE_ID}"
 
 sha1sum /tmp/${GENERATED_FILE}.deb > /tmp/${GENERATED_FILE}.sha1sum
